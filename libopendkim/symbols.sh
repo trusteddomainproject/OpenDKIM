91991ff15f25480994156bd06664ce80221da950
Murray S. Kucherawy <cm-msk@users.sourceforge.net>
2009-11-05 22:57:32 +0000

    Get rid of errant debugging output

--- libopendkim/symbols.sh
+++ libopendkim/symbols.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 #
-# $Id: symbols.sh,v 1.1.2.2 2009/11/05 22:54:46 cm-msk Exp $
+# $Id: symbols.sh,v 1.1.2.3 2009/11/05 22:57:32 cm-msk Exp $
 #
 # Extract from dkim.h the list of symbols we want to export
 
 SOURCEHDRS="dkim.h dkim-test.h"
 SYMLIST=symbols.map
 
-pwd
 grep '^extern' $SOURCEHDRS | \
 	awk '{ for (c = 1; c <= NF; c++) if ($c ~ /dkim_/) { print $c; break; } }' | \
 	sed -e s/\[\*\;\]//g -e s/\[\\\[\\\]\]//g > $SYMLIST
