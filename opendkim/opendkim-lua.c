43bab5eaaeb73591ced7da8d976097e633d0dbb9
Murray S. Kucherawy <cm-msk@users.sourceforge.net>
2009-11-21 02:57:36 +0000

    Checkpoint

--- opendkim/opendkim-lua.c
+++ opendkim/opendkim-lua.c
@@ -1,17 +1,19 @@
 /*
 **  Copyright (c) 2009, The OpenDKIM Project.  All rights reserved.
 **
-**  $Id: opendkim-lua.c,v 1.1.2.2 2009/11/19 22:43:34 cm-msk Exp $
+**  $Id: opendkim-lua.c,v 1.1.2.3 2009/11/21 02:57:36 cm-msk Exp $
 */
 
 #ifndef lint
-static char opendkim_lua_c_id[] = "@(#)$Id: opendkim-lua.c,v 1.1.2.2 2009/11/19 22:43:34 cm-msk Exp $";
+static char opendkim_lua_c_id[] = "@(#)$Id: opendkim-lua.c,v 1.1.2.3 2009/11/21 02:57:36 cm-msk Exp $";
 #endif /* !lint */
 
 #ifdef _FFR_LUA
 
 /* system includes */
 #include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 
 /* LUA includes */
@@ -25,17 +27,92 @@ static char opendkim_lua_c_id[] = "@(#)$Id: opendkim-lua.c,v 1.1.2.2 2009/11/19 
 #include "opendkim-db.h"
 #include "opendkim.h"
 
+/* local data types */
+struct dkimf_lua_io
+{
+	_Bool		lua_io_done;
+	const char *	lua_io_script;
+};
+
+/*
+**  DKIMF_LUA_READER -- "read" a script and make it available to LUA
+**
+**  Parameters:
+**  	l -- LUA state
+**  	data -- pointer to a LUA I/O structure
+**  	size -- size (returned)
+**
+**  Return value:
+**  	Pointer to the data.
+*/
+
+static const char *
+dkimf_lua_reader(lua_State *l, void *data, size_t *size)
+{
+	struct dkimf_lua_io *io;
+
+	assert(l != NULL);
+	assert(io != NULL);
+	assert(size != NULL);
+
+	io = (struct dkimf_lua_io *) data;
+
+	if (io->lua_io_done)
+	{
+		*size = 0;
+		return NULL;
+	}
+	else
+	{
+		io->lua_io_done = TRUE;
+		*size = strlen(io->lua_io_script);
+		return io->lua_io_script;
+	}
+}
+
+/*
+**  DKIMF_LUA_ALLOC -- allocate memory
+**
+**  Parameters:
+**  	ud -- context (not used)
+**  	ptr -- pointer (for realloc())
+**  	osize --
+**  	nsize -- 
+**
+**  Return value:
+**  	Allocated memory, or NULL on failure.
+*/
+
+static void *dkimf_lua_alloc(void *ud, void *ptr, size_t osize, size_t nsize)
+{
+	if (nsize == 0 && osize != 0)
+	{
+		free(ptr);
+		return NULL;
+	}
+	else if (nsize != 0 && osize == 0)
+	{
+		return malloc(nsize);
+	}
+	else
+	{
+		return realloc(ptr, nsize);
+	}
+}
+
 /*
 **  DKIMF_LUA_SIGN_HOOK -- hook to LUA for handling a message while signing
 **
 **  Parameters:
 **  	ctx -- session context, for making calls back to opendkim.c
 **  	script -- script to run
+**  	name -- name of the script (for logging)
 **  	lres -- LUA result structure
 **
 **  Return value:
+**  	1 -- script contains a syntax error
 **  	0 -- success
-**  	-1 -- failure
+**  	-1 -- memory allocation failure
 **
 **  Side effects:
 **  	lres may be modified to relay the script's signing requests, i.e.
@@ -51,33 +128,81 @@ static char opendkim_lua_c_id[] = "@(#)$Id: opendkim-lua.c,v 1.1.2.2 2009/11/19 
 */
 
 int
-dkimf_lua_sign_hook(void *ctx, const char *script,
+dkimf_lua_sign_hook(void *ctx, const char *script, const char *name,
                     struct dkimf_lua_sign_result *lres)
 {
+	struct dkimf_lua_io io;
+	lua_State *l = NULL;
+
 	assert(ctx != NULL);
 	assert(script != NULL);
 	assert(lres != NULL);
 
-	/* XXX -- functions needed from opendkim:
+	io.lua_io_done = FALSE;
+	io.lua_io_script = script;
+
+	l = lua_newstate(dkimf_lua_alloc, NULL);
+	if (l == NULL)
+		return -1;
+
+	/* register functions */
+	/* XXX
 	request DB handle
+	lua_register(l, "odkim_get_dbhandle", dkimf_xs_dbhandle);
+
+	test DB for membership
+	lua_register(l, "odkim_db_check", dkimf_xs_dbquery);
+
+	get a value from a DB
+	lua_register(l, "odkim_db_getvalue", dkimf_xs_dbget);
+
 	request From domain
+	lua_register(l, "odkim_get_fromdomain", dkimf_xs_fromdomain);
+
 	request source hostname/IP address
+	lua_register(l, "odkim_get_source", dkimf_xs_clientinfo);
+
 	domain is signable?
+	lua_register(l, "odkim_signable_domain", dkimf_xs_signabledomain);
+
 	source is signable?
+	lua_register(l, "odkim_signable_ip", dkimf_xs_signableip);
+
 	retrieve header/value
+	lua_register(l, "odkim_get_header", dkimf_xs_getheader);
 	*/
 
 	/* XXX -- functions to provide to LUA:
 	request a signature (domain, selector)
+	lua_register(l, "odkim_sign", dkimf_xs_requestsig);
+
 	request an "l=" tag
+	lua_register(l, "odkim_bodylength", dkimf_xs_bodylength);
+
 	request a "z=" tag
-	get From domain
-	get source host/IP
-	domain is signable?
-	source is signable?
-	retrieve header/value
-	query a DB for membership
+	lua_register(l, "odkim_diagnostics", dkimf_xs_diagnostics);
 	*/
+
+	switch (lua_load(l, dkimf_lua_reader, (void *) &io, name))
+	{
+	  case 0:
+		break;
+
+	  case LUA_ERRSYNTAX:
+		lua_close(l);
+		return 1;
+
+	  case LUA_ERRMEM:
+		lua_close(l);
+		return -1;
+
+	  default:
+		assert(0);
+	}
+
+	lua_close(l);
+
+	return 0;
 }
 
 /*
@@ -87,6 +212,7 @@ dkimf_lua_sign_hook(void *ctx, const char *script,
 **  	ctx -- session context, for making calls back to opendkim.c
 **  	dkim -- DKIM verifying handle
 **  	script -- script to run
+**  	name -- name of the script (for logging)
 **  	lres -- LUA result structure
 **
 **  Return value:
@@ -105,36 +231,80 @@ dkimf_lua_sign_hook(void *ctx, const char *script,
 
 int
 dkimf_lua_verify_hook(void *ctx, DKIM *dkim, const char *script,
-                      struct dkimf_lua_verify_result *lres)
+                      const char *name, struct dkimf_lua_verify_result *lres)
 {
+	struct dkimf_lua_io io;
+	lua_State *l = NULL;
+
 	assert(ctx != NULL);
 	assert(dkim != NULL);
 	assert(script != NULL);
 	assert(lres != NULL);
 
-	/* XXX -- functions needed from libopendkim:
+	io.lua_io_done = FALSE;
+	io.lua_io_script = script;
+
+	l = lua_newstate(dkimf_lua_alloc, NULL);
+	if (l == NULL)
+		return -1;
+
+	/* register functions */
+	/* XXX
 	retrieve Nth signature
+	lua_register(l, "odkim_get_signature", dkimf_xs_getsignature);
+
 	retrieve domain name from signature
+	lua_register(l, "odkim_get_sig_getdomain", dkimf_xs_getsigdomain);
+
 	evaluate signature
+	lua_register(l, "odkim_get_sig_evaluate", dkimf_xs_evaluate);
+
 	did signature use "l="?
+	lua_register(l, "odkim_check_sig_ltag", dkimf_xs_getltag);
+
 	size of body?
+	lua_register(l, "odkim_get_bodylength", dkimf_xs_bodylength);
+
 	canonicalizations?
-	*/
+	lua_register(l, "odkim_get_bodycanon", dkimf_xs_bodycanon);
+	lua_register(l, "odkim_get_hdrcanon", dkimf_xs_hdrcanon);
 
-	/* XXX -- functions needed from opendkim:
 	get policy result
+	lua_register(l, "odkim_get_policy", dkimf_xs_getpolicy);
+
+	set result code
+	lua_register(l, "odkim_set_result_code", dkimf_xs_setresult);
+
+	set reply text
+	lua_register(l, "odkim_set_reply_text", dkimf_xs_setreplytext);
+
+	quarantine?
+	lua_register(l, "odkim_quarantine", dkimf_xs_quarantine);
+
+	redirect
+	lua_register(l, "odkim_redirect", dkimf_xs_redirect);
 	*/
 
-	/* XXX -- functions to provide to LUA:
-	request Nth signature
-	get domain from signature
-	get signature result
-	set result code
-	set reply text
-	quarantine?
-	set quarantine reason
-	redirect
-	*/
+	switch (lua_load(l, dkimf_lua_reader, (void *) &io, name))
+	{
+	  case 0:
+		break;
+
+	  case LUA_ERRSYNTAX:
+		lua_close(l);
+		return 1;
+
+	  case LUA_ERRMEM:
+		lua_close(l);
+		return -1;
+
+	  default:
+		assert(0);
+	}
+
+	lua_close(l);
+
+	return 0;
 }
 
 #endif /* _FFR_LUA */
--- opendkim/opendkim-lua.h
+++ opendkim/opendkim-lua.h
@@ -1,14 +1,14 @@
 /*
 **  Copyright (c) 2009, The OpenDKIM Project.  All rights reserved.
 **
-**  $Id: opendkim-lua.h,v 1.1.2.1 2009/11/19 20:05:05 cm-msk Exp $
+**  $Id: opendkim-lua.h,v 1.1.2.2 2009/11/21 02:57:36 cm-msk Exp $
 */
 
 #ifndef _OPENDKIM_LUA_H_
 #define _OPENDKIM_LUA_H_
 
 #ifndef lint
-static char opendkim_lua_h_id[] = "@(#)$Id: opendkim-lua.h,v 1.1.2.1 2009/11/19 20:05:05 cm-msk Exp $";
+static char opendkim_lua_h_id[] = "@(#)$Id: opendkim-lua.h,v 1.1.2.2 2009/11/21 02:57:36 cm-msk Exp $";
 #endif /* !lint */
 
 /* system includes */
@@ -32,9 +32,10 @@ struct dkimf_lua_verify_result
 };
 
 /* prototypes */
-extern int dkimf_lua_sign_hook __P((void *, const char *,
+extern int dkimf_lua_sign_hook __P((void *, const char *, const char *,
                                     struct dkimf_lua_sign_result *));
 extern int dkimf_lua_verify_hook __P((void *, DKIM *, const char *,
+                                      const char *,
                                       struct dkimf_lua_verify_result *));
 
 #endif /* _OPENDKIM_LUA_H_ */
