0a55412bcc2519849e00c8e56ac152ad31357928
Murray S. Kucherawy <cm-msk@users.sourceforge.net>
2010-05-04 03:24:20 +0000

    Some general fixup

--- opendkim/tests/t-verify-revoked.lua
+++ opendkim/tests/t-verify-revoked.lua
@@ -1,10 +1,10 @@
--- $Id: t-verify-revoked.lua,v 1.1.2.1 2010/05/04 00:55:06 cm-msk Exp $
+-- $Id: t-verify-revoked.lua,v 1.1.2.2 2010/05/04 03:24:20 cm-msk Exp $
 
 -- Copyright (c) 2009, 2010, The OpenDKIM Project.  All rights reserved.
 
--- simple/simple signing test
+-- revoked key verify test
 -- 
--- Confirms that a signature is added with the correct contents.
+-- Confirms that a signature matching a revoked key is reported as such
 
 mt.echo("*** simple/simple verify revoked key")
 
@@ -42,7 +42,9 @@ end
 if mt.header(conn, "DKIM-Signature", "v=1; a=rsa-sha256; c=relaxed/relaxed;\n\td=example.com; s=revoked;\n\th=domainkey-signature:mime-version:received:received:in-reply-to:\n\t references:date:message-id:subject:from:to:cc:content-type;\n\tbh=34lkc8Kvp/4C7BZOHlFJr8XlPeQt1pLHo4V8JljV13c=;\n\tb=sF59xX14kHxUUF2NQs1sF7Jla+dfLKwqCkYRFfLAo1n48oRvlcc3W1nwWU/BEk6mp9\n\t 0ZoylqVfErDrxDTeAEDLwaaPSL+4ZMjvp3WZyuFp2gCoQb5U8INu+vdnNvAgr4hi1Ku4\n\t mEQhR5ncoCxOUWq4e7r1CCIH4DM2fLbZa4ywQ=") ~= nil then
 	error "mt.header(DKIM-Signature) failed"
 end
-
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.header(From) unexpected reply"
+end
 if mt.header(conn, "From", "user@example.com") ~= nil then
 	error "mt.header(From) failed"
 end
