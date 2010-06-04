afc9312814960548bedae1b80f397f1479ca5d46
Murray S. Kucherawy <cm-msk@users.sourceforge.net>
2010-06-04 16:03:23 +0000

    Add a DontSignMailTo test

---
+++ opendkim/tests/t-dontsign.lua
@@ -0,0 +1,100 @@
+-- $Id: t-dontsign.lua,v 1.1 2010/06/04 16:03:23 cm-msk Exp $
+
+-- Copyright (c) 2009, 2010, The OpenDKIM Project.  All rights reserved.
+
+-- "DontSignMailTo" test
+-- 
+-- Confirms that a signature is not added for a specific recipient.
+
+mt.echo("*** DontSignMailTo test")
+
+-- try to start the filter
+binpath = mt.getcwd() .. "/.."
+if os.getenv("srcdir") ~= nil then
+	mt.chdir(os.getenv("srcdir"))
+end
+mt.startfilter(binpath .. "/opendkim", "-x", "t-dontsign.conf")
+mt.sleep(2)
+
+-- try to connect to it
+conn = mt.connect("inet:12345@localhost")
+if conn == nil then
+	error "mt.connect() failed"
+end
+
+-- send connection information
+-- mt.negotiate() is called implicitly
+if mt.conninfo(conn, "localhost", "127.0.0.1") ~= nil then
+	error "mt.conninfo() failed"
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.conninfo() unexpected reply"
+end
+
+-- send envelope macros and sender data
+-- mt.helo() is called implicitly
+mt.macro(conn, SMFIC_MAIL, "j", "t-sign-ss")
+if mt.mailfrom(conn, "user@example.com") ~= nil then
+	error "mt.mailfrom() failed"
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.mailfrom() unexpected reply"
+end
+
+-- send envelope macros and recipient data
+if mt.rcptto(conn, "user@example.net") ~= nil then
+	error "mt.rcptto() failed"
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.rcptto() unexpected reply"
+end
+
+-- send headers
+if mt.header(conn, "From", "user@example.com") ~= nil then
+	error "mt.header(From) failed"
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.header(From) unexpected reply"
+end
+if mt.header(conn, "Date", "Tue, 22 Dec 2009 13:04:12 -0800") ~= nil then
+	error "mt.header(Date) failed"
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.header(Date) unexpected reply"
+end
+if mt.header(conn, "Subject", "Signing test") ~= nil then
+	error "mt.header(Subject) failed"
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.header(Subject) unexpected reply"
+end
+
+-- send EOH
+if mt.eoh(conn) ~= nil then
+	error "mt.eoh() failed"
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.eoh() unexpected reply"
+end
+
+-- send body
+if mt.bodystring(conn, "This is a test!\r\n") ~= nil then
+	error "mt.bodystring() failed"
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.bodystring() unexpected reply"
+end
+
+-- end of message; let the filter react
+if mt.eom(conn) ~= nil then
+	error "mt.eom() failed"
+end
+if mt.getreply(conn) ~= SMFIR_ACCEPT then
+	error "mt.eom() unexpected reply"
+end
+
+-- verify that no signature got added
+if mt.eom_check(conn, MT_HDRINSERT, "DKIM-Signature") or
+   mt.eom_check(conn, MT_HDRADD, "DKIM-Signature") then
+	error "signature added"
+end
