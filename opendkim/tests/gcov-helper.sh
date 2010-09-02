908777022dce265cec6bcda90a28c6ec0552fa8b
Daniel Black <grooverdan@users.sourceforge.net>
2010-09-02 06:56:22 +0000

    lcov for opendkim

---
+++ opendkim/tests/gcov-helper.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+$1
+x=$?
+myname=`basename $1`
+if test x"$OSTYPE" = x"OpenBSD"
+then
+	mv *$myname.bb $myname.bb
+	mv *$myname.bbg $myname.bbg
+	mv *$myname.da $myname.da
+else
+	mv *$myname.gcda $myname.gcda
+	mv *$myname.gcno $myname.gcno
+fi
+exit $x
---
+++ opendkim/tests/lcov-helper.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+$1
+x=$?
+# keep consistant with Makefile.am
+testname=${1/.\/t-}
+testname=${testname//-/_}
+lcov --capture --directory ../.. --output-file $1.info --test-name ${testname} --quiet
+lcov --remove $1.info '/usr/include/*' --output-file $1.info --quiet &
+exit $x
