f7af05375249b06ae75a31e4adb269a57f366088
Daniel Black <grooverdan@users.sourceforge.net>
2010-09-12 01:27:35 +0000

    fix network representation

--- opendkim/tests/t-peer.lua
+++ opendkim/tests/t-peer.lua
@@ -1,4 +1,4 @@
--- $Id: t-peer.lua,v 1.2 2010/09/11 12:51:03 grooverdan Exp $
+-- $Id: t-peer.lua,v 1.3 2010/09/12 01:27:35 grooverdan Exp $
 
 -- Copyright (c) 2010, The OpenDKIM Project.  All rights reserved.
 
@@ -26,7 +26,9 @@ end
 -- to prevent any verification or signing practices
 
 test = {
-	{"localhost", "127.0.0.1", SMFIR_CONTINUE }
+	{"localhost", "2001:db8::91", SMFIR_ACCEPT }
+	, {"localhost", "9001:db8::8:800:200c:417a", SMFIR_CONTINUE }
+	, {"localhost", "127.0.0.1", SMFIR_CONTINUE }
 	, {"localhost", "192.168.1.1", SMFIR_ACCEPT }
 	, {"localhost", "192.168.1.64", SMFIR_CONTINUE }
 	, {"localhost", "192.168.1.128", SMFIR_CONTINUE }
@@ -34,13 +36,13 @@ test = {
 	, {"localhost", "192.168.1.130", SMFIR_CONTINUE }
 	, {"localhost", "192.168.1.131", SMFIR_CONTINUE }
 	, {"localhost", "192.168.1.132", SMFIR_ACCEPT }
-	, {"localhost", "9001:db8:0:0:8:800:200c:417a", SMFIR_CONTINUE }
-	, {"localhost", "2001:db8:0:0:91", SMFIR_CONTINE }
-	, {"localhost", "2001:db8:0:0:128", SMFIR_CONTINUE }
-	, {"localhost", "2001:db8:0:0:129", SMFIR_CONTINUE }
-	, {"localhost", "2001:db8:0:0:130", SMFIR_CONTINUE }
-	, {"localhost", "2001:db8:0:0:131", SMFIR_CONTINUE }
-	, {"localhost", "2001:db8:0:0:132", SMFIR_ACCEPT }
+	, {"localhost", "9001:db8::8:800:200c:417a", SMFIR_CONTINUE }
+	, {"localhost", "2001:db8::91", SMFIR_CONTINE }
+	, {"localhost", "2001:db8::128", SMFIR_CONTINUE }
+	, {"localhost", "2001:db8::129", SMFIR_CONTINUE }
+	, {"localhost", "2001:db8::130", SMFIR_CONTINUE }
+	, {"localhost", "2001:db8::131", SMFIR_CONTINUE }
+	, {"localhost", "2001:db8::132", SMFIR_ACCEPT }
 	}
 for index=1,table.getn(test)
 do
@@ -48,7 +50,15 @@ do
 		error "mt.conninfo() failed"
 	end
 	if mt.getreply(conn) ~= test[index][3] then
-		--stre = "mt.conninfo() unexpected reply " .. test[index][1] .. "(" .. test[index][2] .. ") should be " .. test[index][3]
+		stre = "mt.conninfo() unexpected reply " .. test[index][1] .. "(" .. test[index][2] .. ") should be " 
+		if test[index][3] == SMFIR_CONTINUE then
+			stre = stre .. "SMFIR_CONTINE"
+		else 	
+			stre = stre .. "SMFIR_ACCEPT"
+		end
+		print(stre)
 		error "mt.conninfo() unexpected reply"
+	else
+		print(test[index][1] .. "(" .. test[index][2] .. ") worked")
 	end
 end
