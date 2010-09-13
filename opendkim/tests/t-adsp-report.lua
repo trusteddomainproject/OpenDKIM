9c6477c5570ca53070764479ce0fca5dfe6f0439
Daniel Black <grooverdan@users.sourceforge.net>
2010-09-13 04:19:03 +0000

    adsp test case

---
+++ opendkim/tests/t-adsp-report.lua
@@ -0,0 +1,102 @@
+-- $Id: t-adsp-report.lua,v 1.1 2010/09/13 04:19:03 grooverdan Exp $
+
+-- Copyright (c) 2009, 2010, The OpenDKIM Project.  All rights reserved.
+
+-- reporting adsp failures
+-- 
+-- Confirms that a report is sent adsp requests it
+
+mt.echo("*** test reporting of failured/missing signatures via adsp")
+
+-- setup
+sock = "unix:" .. mt.getcwd() .. "/t-adsp-report.sock"
+binpath = mt.getcwd() .. "/.."
+if os.getenv("srcdir") ~= nil then
+	mt.chdir(os.getenv("srcdir"))
+end
+
+-- try to start the filter
+mt.startfilter(binpath .. "/opendkim", "-x", "t-adsp-report.conf",
+               "-p", sock)
+
+-- try to connect to it
+conn = mt.connect(sock, 40, 0.05)
+if conn == nil then
+	error "mt.connect() failed"
+end
+
+-- send connection information
+-- mt.negotiate() is called implicitly
+if mt.conninfo(conn, "localhost", "127.0.0.1") ~= nil then
+	error "mt.conninfo() failed"
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.conninfo() unexpected reply"
+end
+
+-- send envelope macros and sender data
+-- mt.helo() is called implicitly
+mt.macro(conn, SMFIC_MAIL, "i", "t-verify-revoked")
+if mt.mailfrom(conn, "user@example.com") ~= nil then
+	error "mt.mailfrom() failed"
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.mailfrom() unexpected reply"
+end
+
+if mt.header(conn, "From", "user@example.com") ~= nil then
+	error "mt.header(From) failed"
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.header(From) unexpected reply"
+end
+if mt.header(conn, "Date", "Tue, 22 Dec 2009 13:04:12 -0800") ~= nil then
+	error "mt.header(Date) failed"
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.header(Date) unexpected reply"
+end
+if mt.header(conn, "Subject", "adsp test") ~= nil then
+	error "mt.header(Subject) failed"
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.header(Subject) unexpected reply"
+end
+
+-- send EOH
+if mt.eoh(conn) ~= nil then
+	error "mt.eoh() failed"
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error "mt.eoh() unexpected reply"
+end
+
+-- send body
+if mt.bodystring(conn, "This is a test!\r\n") ~= nil then
+	error "mt.bodystring() failed"
+end
+if mt.getreply(conn) ~= SMFIR_SKIP then
+	print(mt.getreply(conn))
+	error "mt.bodystring() unexpected reply"
+end
+
+-- end of message; let the filter react
+if mt.eom(conn) ~= nil then
+	error "mt.eom() failed"
+end
+if mt.getreply(conn) ~= SMFIR_REJECT then
+	error "mt.eom() unexpected reply"
+end
+
+-- verify that an Authentication-Results header field got added
+-- if not mt.eom_check(conn, MT_HDRINSERT, "Authentication-Results") and
+--    not mt.eom_check(conn, MT_HDRADD, "Authentication-Results") then
+-- 	error "no Authentication-Results added"
+-- end
+-- ar = mt.getheader(conn, "Authentication-Results", 0)
+-- if string.find(ar, "dkim=fail", 1, true) == nil then
+-- 	print(ar)
+-- 	error "incorrect DKIM result"
+-- end
+
+mt.disconnect(conn)
