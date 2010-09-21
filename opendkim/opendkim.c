7b043efc6fad50d34ac401886efa2c44cbd4824e
Murray S. Kucherawy <cm-msk@users.sourceforge.net>
2010-09-21 23:26:00 +0000

    Remember recipients if a stats script is enabled

--- opendkim/opendkim.c
+++ opendkim/opendkim.c
@@ -4,11 +4,11 @@
 **
 **  Copyright (c) 2009, 2010, The OpenDKIM Project.  All rights reserved.
 **
-**  $Id: opendkim.c,v 1.214 2010/09/21 21:01:03 cm-msk Exp $
+**  $Id: opendkim.c,v 1.215 2010/09/21 23:26:00 cm-msk Exp $
 */
 
 #ifndef lint
-static char opendkim_c_id[] = "@(#)$Id: opendkim.c,v 1.214 2010/09/21 21:01:03 cm-msk Exp $";
+static char opendkim_c_id[] = "@(#)$Id: opendkim.c,v 1.215 2010/09/21 23:26:00 cm-msk Exp $";
 #endif /* !lint */
 
 #include "build-config.h"
@@ -8818,6 +8818,9 @@ mlfi_envrcpt(SMFICTX *ctx, char **envrcpt)
 	    || conf->conf_setupscript != NULL
 	    || conf->conf_screenscript != NULL
 	    || conf->conf_finalscript != NULL
+# ifdef _FFR_STATSEXT
+	    || conf->conf_statsscript != NULL
+# endif /* _FFR_STATSEXT */
 #endif /* USE_LUA */
 	   )
 	{
