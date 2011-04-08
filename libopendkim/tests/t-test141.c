27d924207f83cc9d18d53eeac7a3b0b6759b7a49
Murray S. Kucherawy <msk@opendkim.org>
2011-04-07 17:49:13 -0700

    Add unit test for this bug fix

---
+++ libopendkim/tests/t-test141.c
@@ -0,0 +1,178 @@
+/*
+**  Copyright (c) 2005-2008 Sendmail, Inc. and its suppliers.
+**    All rights reserved.
+**
+**  Copyright (c) 2009, 2011, The OpenDKIM Project.  All rights reserved.
+*/
+
+#ifndef lint
+static char t_test141_c_id[] = "@(#)$Id: t-test52.c,v 1.2 2009/12/08 19:14:27 cm-msk Exp $";
+#endif /* !lint */
+
+#include "build-config.h"
+
+/* system includes */
+#include <sys/types.h>
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+
+#ifdef USE_GNUTLS
+# include <gnutls/gnutls.h>
+#endif /* USE_GNUTLS */
+
+/* libopendkim includes */
+#include "../dkim.h"
+#include "t-testdata.h"
+
+#define	MAXHEADER	4096
+
+#define SIG2 "v=1; a=rsa-sha256; c=relaxed/simple; d=example.com; s=test;\r\n\tt=1172620939; bh=yHBAX+3IwxTZIynBuB/5tlsBInJq9n8qz5fgAycHi80=;\r\n\th=From:To:Date:Subject:Message-ID;\r\n\tz=From:=20Murray=20S.=20Kucherawy=20<msk@sendmail.com>|To:=20Sendma\r\n\t il=20Test=20Address=20<sa-test@sendmail.net>|Date:=20Thu,=2005=20M\r\n\t ay=202005=2011:59:09=20-0700|Subject:=20DKIM=20test=20message|Mess\r\n\t age-ID:=20<439094BF.5010709@sendmail.com>;\r\n\tb=H82IO1rXD0LnIrnRx6eCwWmGzuPqtxdmU54zvuuLah/HAzDM/rRn8nWC1usCU8buK\r\n\t GNCyrTzuNTOTB2fWI0XuMAsSaGI9fw/VO3sfGf+4yJn98U6BpK6iPqLaIdnexyb7Zo\r\n\t DK5SW7Bi/90vMNViofUf4Uoy+DiUKXHzUpX5BYiQ="
+
+/*
+**  MAIN -- program mainline
+**
+**  Parameters:
+**  	The usual.
+**
+**  Return value:
+**  	Exit status.
+*/
+
+int
+main(int argc, char **argv)
+{
+	u_int flags = 0;
+	DKIM_STAT status;
+	uint64_t fixed_time;
+	DKIM *dkim;
+	DKIM_LIB *lib;
+	dkim_sigkey_t key;
+	unsigned char hdr[MAXHEADER + 1];
+
+#ifdef USE_GNUTLS
+	(void) gnutls_global_init();
+#endif /* USE_GNUTLS */
+
+	/* instantiate the library */
+	lib = dkim_init(NULL, NULL);
+	assert(lib != NULL);
+
+	if (!dkim_libfeature(lib, DKIM_FEATURE_SHA256))
+	{
+		printf("*** relaxed/simple rsa-sha256 signing with z-tags SKIPPED\n");
+		dkim_close(lib);
+		return 0;
+	}
+	printf("*** relaxed/simple rsa-sha256 signing with constrained z-tags\n");
+
+#ifdef TEST_KEEP_FILES
+	/* set flags */
+	flags = (DKIM_LIBFLAGS_TMPFILES|DKIM_LIBFLAGS_KEEPFILES);
+	(void) dkim_options(lib, DKIM_OP_SETOPT, DKIM_OPTS_FLAGS, &flags,
+	                    sizeof flags);
+#endif /* TEST_KEEP_FILES */
+
+	flags |= (DKIM_LIBFLAGS_TMPFILES|DKIM_LIBFLAGS_ZTAGS);
+	(void) dkim_options(lib, DKIM_OP_SETOPT, DKIM_OPTS_FLAGS, &flags,
+	                    sizeof flags);
+	key = KEY;
+
+	dkim = dkim_sign(lib, JOBID, NULL, key, SELECTOR, DOMAIN,
+	                 DKIM_CANON_RELAXED, DKIM_CANON_SIMPLE,
+	                 DKIM_SIGN_RSASHA256, -1L, &status);
+	assert(dkim != NULL);
+
+	/* fix signing time */
+	fixed_time = 1172620939;
+	(void) dkim_options(lib, DKIM_OP_SETOPT, DKIM_OPTS_FIXEDTIME,
+	                    &fixed_time, sizeof fixed_time);
+
+	/* arrange to sign specific headers */
+	(void) dkim_options(lib, DKIM_OP_SETOPT, DKIM_OPTS_SIGNHDRS,
+	                    dkim_should_signhdrs, sizeof(char **));
+
+	status = dkim_header(dkim, HEADER02, strlen(HEADER02));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_header(dkim, HEADER03, strlen(HEADER03));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_header(dkim, HEADER04, strlen(HEADER04));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_header(dkim, HEADER05, strlen(HEADER05));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_header(dkim, HEADER06, strlen(HEADER06));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_header(dkim, HEADER07, strlen(HEADER07));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_header(dkim, HEADER08, strlen(HEADER08));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_header(dkim, HEADER09, strlen(HEADER09));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_eoh(dkim);
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_body(dkim, BODY00, strlen(BODY00));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_body(dkim, BODY01, strlen(BODY01));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_body(dkim, BODY01A, strlen(BODY01A));
+	assert(status == DKIM_STAT_OK);
+	status = dkim_body(dkim, BODY01B, strlen(BODY01B));
+	assert(status == DKIM_STAT_OK);
+	status = dkim_body(dkim, BODY01C, strlen(BODY01C));
+	assert(status == DKIM_STAT_OK);
+	status = dkim_body(dkim, BODY01D, strlen(BODY01D));
+	assert(status == DKIM_STAT_OK);
+	status = dkim_body(dkim, BODY01E, strlen(BODY01E));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_body(dkim, BODY02, strlen(BODY02));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_body(dkim, BODY03, strlen(BODY03));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_body(dkim, BODY04, strlen(BODY04));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_body(dkim, BODY03, strlen(BODY03));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_body(dkim, BODY03, strlen(BODY03));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_body(dkim, BODY05, strlen(BODY05));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_body(dkim, BODY03, strlen(BODY03));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_body(dkim, BODY03, strlen(BODY03));
+	assert(status == DKIM_STAT_OK);
+
+	status = dkim_eom(dkim, NULL);
+	assert(status == DKIM_STAT_OK);
+
+	memset(hdr, '\0', sizeof hdr);
+	status = dkim_getsighdr(dkim, hdr, sizeof hdr,
+	                        strlen(DKIM_SIGNHEADER) + 2);
+	assert(status == DKIM_STAT_OK);
+	assert(strcmp(SIG2, hdr) == 0);
+
+	status = dkim_free(dkim);
+	assert(status == DKIM_STAT_OK);
+
+	dkim_close(lib);
+
+	return 0;
+}
