a5d7f359ab77151cb9fef8dcda9b792bcfa3e5f6
Murray S. Kucherawy <msk@trusteddomain.org>
2012-10-29 23:09:50 -0700

    Wrong attribution

--- autobuild/autobuild.c
+++ autobuild/autobuild.c
@@ -1,5 +1,6 @@
 /*
-**  Copyright (c) 2012-present, Facebook, Inc.  All rights reserved.
+**  Copyright (c) 2012-present, The Trusted Domain Project.
+**  	All rights reserved.
 */
 
 #include <sys/param.h>
