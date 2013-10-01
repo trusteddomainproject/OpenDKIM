7cf3a6e4148b7265341a4c2e01b043a9c89e5777
Murray S. Kucherawy <msk@trusteddomain.org>
2013-09-30 22:28:14 -0700

    Update copyright

--- opendkim/opendkim-dns.h
+++ opendkim/opendkim-dns.h
@@ -2,7 +2,7 @@
 **  Copyright (c) 2008 Sendmail, Inc. and its suppliers.
 **    All rights reserved.
 **
-**  Copyright (c) 2009-2012, The Trusted Domain Project.  All rights reserved.
+**  Copyright (c) 2009-2013, The Trusted Domain Project.  All rights reserved.
 */
 
 #ifndef _OPENDKIM_DNS_H_
