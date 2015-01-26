71565500dfe2c9c53a94ec0ed7d9da3ad248fb2a
Murray S. Kucherawy <msk@trusteddomain.org>
2015-01-26 09:05:04 -0800

    Update copyright

--- libopendkim/dkim-atps.c
+++ libopendkim/dkim-atps.c
@@ -1,5 +1,6 @@
 /*
-**  Copyright (c) 2010-2012, The Trusted Domain Project.  All rights reserved.
+**  Copyright (c) 2010-2012, 2015, The Trusted Domain Project.
+**  	All rights reserved.
 */
 
 #include "build-config.h"
