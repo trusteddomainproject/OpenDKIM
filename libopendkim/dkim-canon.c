a65442161a6e59de49b3db9091754174e363c392
Murray S. Kucherawy <msk@trusteddomain.org>
2015-10-07 00:23:33 -0700

    LIBOPENDKIM: Re-fix bug #226: Deal with header fields that are wrapped before there's any content.  Somehow the wrong fix was committed.  Originally reported by Alessandro Vesely; re-reported by David Stevenson.

--- libopendkim/dkim-canon.c
+++ libopendkim/dkim-canon.c
@@ -388,7 +388,7 @@ dkim_canon_header_string(struct dkim_dstring *dstr, dkim_canon_t canon,
 		}
 
 		/* skip all spaces before first word */
-		while (*p != '\0' && DKIM_ISWSP(*p))
+		while (*p != '\0' && DKIM_ISLWSP(*p))
 			p++;
 
 		space = FALSE;				/* just saw a space */
