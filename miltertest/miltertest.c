4d05b437a4dcbfb917d22f49f8ac935159c4213d
Matt Domsch <matt@domsch.com>
2020-04-26 13:46:46 -0500

    Minor updates for lua 5.3
    Simple changes to all the LUA_VERSION_NUM == 502 tests throughout the
    code to make them >= 502, to allow for lua 5.3.  In addition, lua 5.3
    changed the signature for lua_dump() to add a strip parameter.  In
    those cases, call the new signature with strip = 0 to retain the old
    behavior.
    
    This is necessary to properly build on centos 8, Fedora 31 and above
    with lua 5.3.  Based on a patch in the Fedora tree by Breno Brand
    Fernandes brandfbb@gmail.com which replaced the problematic
    calls with one for the new version of lua.  This patch maintains
    backwards compatibility with older versions of lua, should that
    be necessary.

--- miltertest/miltertest.c
+++ miltertest/miltertest.c
@@ -4009,12 +4009,12 @@ main(int argc, char **argv)
 	}
 
 	/* register functions */
-#if LUA_VERSION_NUM == 502
-        luaL_newlib(l, mt_library);
+#if LUA_VERSION_NUM >= 502
+	luaL_newlib(l, mt_library);
 	lua_setglobal(l, "mt");
-#else /* LUA_VERSION_NUM == 502 */
+#else /* LUA_VERSION_NUM >= 502 */
 	luaL_register(l, "mt", mt_library);
-#endif /* LUA_VERSION_NUM == 502 */
+#endif /* LUA_VERSION_NUM >= 502 */
 	lua_pop(l, 1);
 
 	/* register constants */
@@ -4163,13 +4163,13 @@ main(int argc, char **argv)
 	lua_setglobal(l, "SMFIF_SETSYMLIST");
 #endif /* SMFIF_SETSYMLIST */
 
-#if LUA_VERSION_NUM == 502
+#if LUA_VERSION_NUM >= 502
 	switch (lua_load(l, mt_lua_reader, (void *) &io,
 	                 script == NULL ? "(stdin)" : script, NULL))
-#else /* LUA_VERSION_NUM == 502 */
+#else /* LUA_VERSION_NUM >= 502 */
 	switch (lua_load(l, mt_lua_reader, (void *) &io,
 	                 script == NULL ? "(stdin)" : script))
-#endif /* LUA_VERSION_NUM == 502 */
+#endif /* LUA_VERSION_NUM >= 502 */
 	{
 	  case 0:
 		break;
