2c2b37704309a8ab32cd6a888bfb2036f8183fff
Richard Hansen <rhansen@rhansen.org>
2020-10-08 21:20:36 -0400

    Delete dkim_res_nslist(), rbl_res_nslist()
    These functions are useless: If HAVE_RES_SETSERVERS is defined they
    don't compile due to a handful of obvious bugs. If HAVE_RES_SETSERVERS
    is not defined they are no-ops. For over 6 years HAVE_RES_SETSERVERS
    was never defined by configure.ac due to a silly typo. Because nobody
    seemed to notice that they were no-ops, I doubt anyone will miss them
    when they're gone.

--- libopendkim/dkim-dns.h
+++ libopendkim/dkim-dns.h
@@ -13,7 +13,6 @@
 extern int dkim_res_cancel __P((void *, void *));
 extern void dkim_res_close __P((void *));
 extern int dkim_res_init __P((void **));
-extern int dkim_res_nslist __P((void *, const char *));
 extern int dkim_res_query __P((void *, int, unsigned char *, unsigned char *,
                                size_t, void **));
 extern int dkim_res_waitreply __P((void *, void *, struct timeval *,
