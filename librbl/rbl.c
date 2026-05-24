6e65b22e734ad5e1dfcacf9f1939b58f7d5bd43f
Dan Mahoney <github@gushi.org>
2026-05-23 19:14:17 -0700

    librbl, libopendkim: fix struct state -> struct __res_state typo
    gcc 14 -Werror rejects the old non-POSIX name. All three occurrences
    are local variables cast from void *srv, so there is no ABI impact.
    
    Fixes #224.

--- librbl/rbl.c
+++ librbl/rbl.c
@@ -258,7 +258,7 @@ rbl_res_nslist(void *srv, const char *nslist)
 # ifdef AF_INET6
 	struct sockaddr_in6 in6;
 # endif /* AF_INET6 */
-	struct state *res;
+	struct __res_state *res;
 	res_sockaddr_union nses[MAXNS];
 
 	assert(srv != NULL);
@@ -329,7 +329,7 @@ void
 rbl_res_close(void *srv)
 {
 #ifdef HAVE_RES_NINIT
-	struct state *res;
+	struct __res_state *res;
 
 	res = srv;
 
