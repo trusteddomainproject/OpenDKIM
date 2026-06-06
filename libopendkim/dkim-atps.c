15be2d57d6824d98172471e87c974f1e7f362f83
Dan Mahoney <github@gushi.org>
2026-06-06 14:37:35 -0700

    libopendkim: add T_DNAME fallback define in dkim-report.c and dkim-atps.c

--- libopendkim/dkim-atps.c
+++ libopendkim/dkim-atps.c
@@ -52,6 +52,9 @@ extern void dkim_error (DKIM *, const char *, ...);
 #ifndef T_RRSIG
 # define T_RRSIG		46
 #endif /* ! T_RRSIG */
+#ifndef T_DNAME
+# define T_DNAME		39
+#endif /* ! T_DNAME */
 #ifndef MAX
 # define MAX(x,y)		((x) > (y) ? (x) : (y))
 #endif /* ! MAX */
